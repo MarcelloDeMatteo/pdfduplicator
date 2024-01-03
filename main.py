de84db2f947962f6d01167adaf110da4d17d99b9
Marcello De Matteo <marcello@dematteo.ch>
2024-01-03 13:29:52 +0100

    Duplicator angepasst, Seiten in Range 1bis2 werden duppliziert

--- main.py
+++ main.py
@@ -4,36 +4,30 @@ from tkinter import filedialog, simpledialog
 
 def main():
     root = tk.Tk()
-    root.withdraw() # Versteckt das Hauptfenster von Tkinter
+    root.withdraw()
 
-    # Öffnen eines Dialogfensters zur Auswahl der Eingabedatei
     input_path = filedialog.askopenfilename(title="Wählen Sie die PDF-Datei aus", filetypes=[("PDF Files", "*.pdf")])
     if not input_path:
         return
 
-    # Abfrage der Anzahl der Duplikate
-    number_of_copies = simpledialog.askinteger("Anzahl der Duplikate", "Geben Sie die Anzahl der Duplikate ein:", minvalue=1, maxvalue=1000)
-    if not number_of_copies:
+    number_of_duplicates = simpledialog.askinteger("Anzahl der Duplikate", "Geben Sie die Anzahl der Duplikate ein:", minvalue=1, maxvalue=400)
+    if not number_of_duplicates:
         return
 
-    # Öffnen eines Dialogfensters zur Festlegung des Ausgabepfads
     output_path = filedialog.asksaveasfilename(defaultextension=".pdf", title="Speichern als", filetypes=[("PDF Files", "*.pdf")])
     if not output_path:
         return
 
-    # Öffnen des PDFs
     with open(input_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
         writer = PyPDF2.PdfWriter()
 
-        # Wählen Sie die zu duplizierende Seite (z.B. Seite 1)
-        page = reader.pages[0]
+        # Duplizieren der ersten beiden Seiten
+        for _ in range(number_of_duplicates):
+            for page_number in range(2):
+                page = reader.pages[page_number]
+                writer.add_page(page)
 
-        # Duplizieren Sie die Seite entsprechend der Anzahl
-        for _ in range(number_of_copies):
-            writer.add_page(page)
-
-        # Schreiben Sie das neue PDF
         with open(output_path, 'wb') as output_pdf:
             writer.write(output_pdf)
 
